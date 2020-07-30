f763e11801f90747e5c42c77c2d30e7ac33ea8ef
Elizabeth Fuentes Leone <elizabethfuentesleone@MacBook-Air-de-Elizabeth.local>
2020-07-29 22:16:17 -0400

    “ML”

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# busqueda_portal_inmo
+# Búsqueda sencilla en el portal inmobiliario o en cualquier pagina web 
+## Un programa sencillo que permite hacer búsqueda masiva de arriendos o compras de propiedades separándolas por comunas
+
+## Revisa mi programa acá [GitHub Pages](https://github.com/elizabethfuentes12/busqueda_portal_inmo/blob/master/busqueda_portal.py).
+
--- busqueda_portal.py
+++ busqueda_portal.py
@@ -6,17 +6,20 @@ from selenium.webdriver.support.ui import WebDriverWait
 import pandas as pd
 
 # %%
-comuna=["Providencia, RM (Metropolitana)","Santiago, RM (Metropolitana)","Ñuñoa, RM (Metropolitana)","San Miguel, RM (Metropolitana)","Independencia, RM (Metropolitana)"]
+#Se crea una serie con las comunas que se deseea explorar
+
+comuna=["Providencia, RM (Metropolitana)","Santiago, RM (Metropolitana)","Ñuñoa, RM (Metropolitana)","La Reina, RM (Metropolitana)","Las Condes, RM (Metropolitana)"]
+
 df_final=[]
 for n in comuna:
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.implicitly_wait(3)
     driver.get("https://www.portalinmobiliario.com") #Cargamos la página principal
 
-    #Buscamos el boton para seleccionar, luego seleccionamos "arriendos"
+    #Buscamos el boton para seleccionar, luego seleccionamos "arriendos" o "venta"
     driver.find_element_by_id('operations-dropdown').find_element_by_class_name('searchbox-dropdown__content').click()
     #driver.find_element_by_css_selector('li[data-id="arriendo"]').click()
-    driver.find_element_by_css_selector('li[data-id="venta"]').click()
+    driver.find_element_by_css_selector('li[data-id="venta"]').click() #Aca debes cambiar por arriendo o venta
 
     #Buscamos el cuadro de texto para ingresar la comuna
     where = driver.find_element_by_class_name('searchbox-autocomplete__field')
@@ -24,8 +27,6 @@ for n in comuna:
     where.send_keys(n) #esto desplega una lista de autocompletado con sugerencias
     driver.find_element_by_css_selector('li.searchbox-autocomplete-list__item').click() #seleccionamos la primera opcion (que será la que escribimos)
     driver.find_element_by_id('search-submit').click() #Buscar
-
-
 
     #Ahora con la busqueda cargada extraemos 100 resultados
 
@@ -58,6 +59,7 @@ for n in comuna:
     df["Comuna"] = str(n)
     df["Valor UF"]=0
     df["Valor Pesos"]=0
+    #Los valores vienen mixtos por lo que se debe serparar por Pesos y otro por UFs
     for j in range (len(df)):
         l=int(j+1)
         x=int(df["Precio"][j:l].str.find("UF "))
@@ -70,75 +72,14 @@ for n in comuna:
     else: 
         df_final=df.copy()
 
+#Lo puedes ordenar por Valor UF o por Precio en pesos, debes recordar que el que no tenga ese tipo de valor sera igual a 0
+
 df_final=df_final.sort_values(by=["Valor UF"]).reset_index(drop=True)
+df_final=df_final.sort_values(by=["Valor Pesos"]).reset_index(drop=True)
+
+archivo="tu_archivo.csv"
+
+df_final.to_csv(archivo,index=False,sep=";")
+
+#Listo consigue el mejor precio
     
-
-# %%
-#df_final=df_final.sort_values(by=["Valor UF"]).reset_index(drop=True)
-df_final=df_final.sort_values(by=["Valor UF"]).reset_index(drop=True)
-df_final
-# %%
-#X=int(df_final["Precio"][0:1].str.find("UF "))
-#df_final["Precio"][0][6:]
-df_final["Valor Pesos"]=0
-for j in range (len(df_final)):
-        l=int(j+1)
-        x=int(df_final["Precio"][j:l].str.find("UF "))
-        df_final["Valor UF"][j]=df_final["Precio"][j][x:]
-        y=int(df_final["Precio"][j:l].str.find("$ "))
-        df_final["Valor Pesos"][j]=df_final["Precio"][j][y:]
-
-df_final=df_final.sort_values(by=["Valor UF"]).reset_index(drop=True)
-df_final
-
-# %%
-df_final.to_csv("portal_comunas.csv",index=False,sep=";")
-
-# %%
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.implicitly_wait(3)
-driver.get("https://www.portalinmobiliario.com") #Cargamos la página principal
-
-#Buscamos el boton para seleccionar, luego seleccionamos "arriendos"
-driver.find_element_by_id('operations-dropdown').find_element_by_class_name('searchbox-dropdown__content').click()
-driver.find_element_by_css_selector('li[data-id="arriendo"]').click()
-
-#Buscamos el cuadro de texto para ingresar la comuna
-where = driver.find_element_by_class_name('searchbox-autocomplete__field')
-where.clear()
-where.send_keys("Santiago, RM (Metropolitana)") #esto desplega una lista de autocompletado con sugerencias
-driver.find_element_by_css_selector('li.searchbox-autocomplete-list__item').click() #seleccionamos la primera opcion (que será la que escribimos)
-driver.find_element_by_id('search-submit').click() #Buscar
-
-
-
-#Ahora con la busqueda cargada extraemos 100 resultados
-
-nb=[]
-
-
-while len(nb) < 2: #hasta que el DF tenga mínimo 100 elementos.
-    elems = driver.find_elements_by_class_name("results-item") # el contenedor de los resultados.
-    for e in elems:
-        try:
-            print(e.find_element_by_css_selector('a.item__info-link').get_attribute("href"))
-            #print(e.driver.find_element_by_partial_link_text("https://www.portalinmobiliario.com/venta/").text) #MLC522440361 > div.item__info-container > div > div > a.item__info-link
-            titulo=e.find_element_by_tag_name("h2.item_subtitle").text
-            direccion=e.find_elements_by_tag_name("span")
-            info=e.find_element_by_tag_name("div.item__attrs").text
-            superficie=info.split("|")[0]
-            dormitorios=info.split("|")[1]
-            baños=info.split("|")[2]
-            precio=e.find_element_by_tag_name("div.price__container").text
-            nb.append({"Precio":precio,"Direccion":direccion[2].text,"Superficie":superficie,"Dormitorios":dormitorios,"Baños":baños})
-        except:
-            print("Error")
-            break
-            
-    driver.find_element_by_css_selector('.andes-pagination__button--next').click() #vamos a la siguiente página.
-
-#df = pd.DataFrame(nb)
-#df = df[:100]
-driver.close()
-
-# %%
